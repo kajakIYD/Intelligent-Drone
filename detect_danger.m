396a8696ff2d179ebaec841106f17a6d9ce65ebc
Nightrray <bienias.krzysztof94@gmail.com>
2019-05-22 22:28:19 +0200

    Funkcja odpowiedzialna za %wykrycie zagrożenia. aktualizujemy wartosci predykatow za pomocą parametru pozycji drona oraz drogi do przejścia

---
+++ detect_danger.m
@@ -0,0 +1,11 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Funkcja odpowiedzialna za %wykrycie zagro¿enia. aktualizujemy wartosci predykatow za pomoc¹ parametru pozycji drona oraz drogi do przejœcia
+%opdpowiada to funkcji TELL - bazy wiedzy bêdzie siê zmieniaæ w ka¿dym kroku symulacji
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [predicates] = detect_danger(drone_position, cube_to_pass) 
+    %cube_to_pass - wycinek srodowiska do sprawdzenia (tam sa 'r' 'g')
+    
+
+    predicates = [];
+end
