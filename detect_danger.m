5937f0b69afb2a5ac82ade26c2ee5b14ae205e1e
Nightrray <bienias.krzysztof94@gmail.com>
2019-05-27 21:34:40 +0200

    Sprawdzenie czy dron osiagnal wysokosc pilota (orientacja w przod i tyl

--- detect_danger.m
+++ detect_danger.m
@@ -3,9 +3,18 @@
 %opdpowiada to funkcji TELL - bazy wiedzy bêdzie siê zmieniaæ w ka¿dym kroku symulacji
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [predicates] = detect_danger(drone_position, cube_to_pass) 
+function [predicates] = detect_danger(drone, environment, optimal_path, predicates) 
     %cube_to_pass - wycinek srodowiska do sprawdzenia (tam sa 'r' 'g')
     
-
-    predicates = [];
+    %%%%%TODO%%%%%
+    %trzeba sprawdzac czy jestesmy na granicy srodowiska, zeby nie probowac
+    %wykryc radaru albo guna tam gdzie juz nie ma srodowiska
+    
+    %%predicates : radar wykryty, gun wykryty
+    
+    if (drone.pilot_position_achieved == true)
+        predicates = detect_danger_backward_orientation(drone, environment, optimal_path, predicates);
+    else
+        predicates = detect_danger_forward_orientation(drone, environment, optimal_path, predicates);
+    end
 end
