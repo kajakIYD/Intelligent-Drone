b66885d7ffb5e5253e91297dd2583a5dd43e1f57
Nightrray <bienias.krzysztof94@gmail.com>
2019-05-27 21:33:28 +0200

    Dodanie sprawdzeń na osi Y i Z

--- check_coordinates.m
+++ check_coordinates.m
@@ -6,30 +6,33 @@
 
 function [x_start, x_end, y_start, y_end, z_start, z_end] = check_coordinates(drone, N, H)
 
-    x_start = drone.position.x - drone.sensor_range;
+    x_start = drone.position.x;% - drone.sensors_range;
     if (x_start < 1)
        x_start = 1; 
     end
-    x_end = drone.position.x + drone.sensor_range;
+    x_end = drone.position.x + drone.sensors_range;
     if (x_end < 1)
        x_end = 1; 
     end
     if (x_end > N)
         x_end = N; 
     end
-    y_start = drone.position.y - drone.sensor_range;
+    y_start = drone.position.y; % - drone.sensors_range;
     if (y_start < 1)
        y_start = 1; 
     end
-    y_end = drone.position.x + drone.sensor_range;
+    y_end = drone.position.y + drone.sensors_range;
+      if (y_end < 1)
+       y_end = 1; 
+    end
     if (y_end > N)
        y_end = N; 
     end
-    z_start = drone.position.z - drone.sensor_range;
+    z_start = drone.position.z; % - drone.sensors_range;
     if (z_start < 1)
        z_start = 1; 
     end
-    z_end = drone.position.x + drone.sensor_range;
+    z_end = drone.position.z + drone.sensors_range;
     if (z_end > N)
        z_end = H; 
     end
