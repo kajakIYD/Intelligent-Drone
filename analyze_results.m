0b728ffce57983e4fa988b7b1d3d13f22676ce96
Nightrray <bienias.krzysztof94@gmail.com>
2019-06-18 20:11:54 +0200

    Dodanie obslugi komunikatow (message zbierajacych informacje o czasie, predkosci, czy misja sie wykonala poprawnie itp)

--- analyze_results.m
+++ analyze_results.m
@@ -1,4 +1,21 @@
-function analyze_results(drone, simulation_time)
+function [message] = analyze_results(drone, moves_and_states, simulation_time, message)
 %analiza rezultatów (dron w czasie)
-
+    
+    if (drone.if_return_to_start == true)
+        message  = [message 'Misja zakoñczona powodzeniem' newline];
+    else
+        message  = [message 'Misja zakoñczona niepowodzeniem' newline];
+    end
+    
+    message  = [message 'Symulacja trwa³a: ' num2str(simulation_time) newline];
+    
+    suma = 0;
+    for move = moves_and_states.moves
+        suma = suma + abs(move.x) + abs(move.y) + abs(move.z);
+    end
+    
+    avg_speed = suma / simulation_time;
+    message = [message 'Œrednia prêdkoœæ: ' num2str(avg_speed) newline];
+    
+    message  = [message 'Energia pocz¹tkowa: ' num2str(drone.initial_energy) ' Pozosta³o energii: ' num2str(drone.energy)];
 end
