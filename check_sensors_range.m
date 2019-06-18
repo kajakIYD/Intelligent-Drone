7675acc527c15bf90c2d67ba13ec64f66d52a26f
Nightrray <bienias.krzysztof94@gmail.com>
2019-06-18 20:25:10 +0200

    Zmiana zasiegu czujnikow w zaleznosci od wysokosci

---
+++ check_sensors_range.m
@@ -0,0 +1,11 @@
+function drone = check_sensors_range(drone)
+
+    if (drone.position.z > 1 && drone.position.z <= 3)
+        drone.sensors_range = 1;
+    elseif (drone.position.z > 3 && drone.position.z <= 7)
+        drone.sensors_range = 2;
+    elseif(drone.position.z > 7)
+        drone.sensors_range = 3;
+    end
+
+end
