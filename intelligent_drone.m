da4d264583a7e4476bbf66ce343217f0098a5542
Nightrray <bienias.krzysztof94@gmail.com>
2019-06-23 16:15:24 +0200

    Dodanie zmiennej move to make from backwards. Zmiana koncepcji... ruch powrotny bedzie odbywal sie po tej samej sciezce co ruch w przod tylko z przeciwnym znakiem

--- intelligent_drone.m
+++ intelligent_drone.m
@@ -3,12 +3,15 @@
 %bêd¹ poszczególne funkcje programu
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% clear all
 close all
 
 drone = struct %deklaracja struktury (dron bêdzie stuktur¹ przechowuj¹c¹ informacjê 
 % o jego po³o¿eniu, wysokoœci, prêdkoœci, energii, zasiêgu czujnika)
 
-[drone, N, H] = get_simulation_parameters(drone, 20, 10, 20, 10, 100, 20);
+[drone, N, H, pilot_position] = get_simulation_parameters(drone, 10, 10, 10, 10, 50, 20);
+%pilot_position = pilot_position_backup;
+%drone.initial_position = drone_init_pos_backup;
 
 drone.pilot_position_achieved=false; %pozycja pilota osi¹gniêta (pocz¹tkowo ustawione na wartosæ false)
 
@@ -17,6 +20,7 @@ drone.if_return_to_start = false; %parametr czy powróci³ do startu (pozycji star
 environment = zeros(N, N, H); %inicjalizacja œrodowiska o wymiarach NxNxH
 
 environment = initialize_guns_and_radars(environment);
+%environment = env_backup;
 
 predicates = struct %predykaty bêd¹ strukturami z przyjêtymi parametrami zdanie oraz wartoœæ logiczna
 %predicates.q = "Radar detected"/"Gun detected" // zdanie
@@ -35,6 +39,8 @@ predicates = struct;
 
 message = '';
 
+move_to_make_from_backwards = 0;
+pilot_position_achieved_time = 0;
 
 while ( drone.energy > 0  && drone.if_return_to_start ~= true ) %dopóki energia drona jest >0 i nie powróci³ do pozycji pocz¹tkowej
     %%TODO ruch w gore: 
@@ -48,24 +54,27 @@ while ( drone.energy > 0  && drone.if_return_to_start ~= true ) %dopóki energia 
     [predicates, drone] = check_pilot_presence(drone, pilot_position, predicates);
     [optimal_path, predicates] = calculate_optimal_path(drone, predicates);
     
-    if ( drone.pilot_position_achieved == true)
-        disp('a');
-    end
-    
     drone = check_sensors_range(drone);
     predicates = detect_danger(drone, environment, optimal_path, predicates); %wykryj zagro¿enie aktualizujemy wartosci predykatow za pomoc¹ parametru pozycji drona oraz drogi do przejœcia
 
     %pocz¹tek tworzenia bazy wiedzy (odpowiada to funkcji TELL - baza
     %wiedzy bêdzie siê zmieniaæ w ka¿dym kroku symulacji
     
-    [move_to_make] = inference(predicates, drone); 
-    % funkcja odpowiedzialna za pobranie aktualnych predykatów oraz
-    % parametrów drona (przypisuje do zmiennych ruch do zrobienia i
-    % aktualny po³o¿enie drona)
-    %odpowiada to funkcji ASK pytamy sie czy jestemy pod dzia³aniem radaru,
-    %czy gun
-    
-    
+    if (drone.pilot_position_achieved == false)
+        [move_to_make] = inference(predicates, drone); 
+        % funkcja odpowiedzialna za pobranie aktualnych predykatów oraz
+        % parametrów drona (przypisuje do zmiennych ruch do zrobienia i
+        % aktualny po³o¿enie drona)
+            %odpowiada to funkcji ASK pytamy sie czy jestemy pod dzia³aniem radaru,
+            %czy gun
+        pilot_position_achieved_time = simulation_time;
+    else
+        move_to_make = moves_and_states.moves(pilot_position_achieved_time - move_to_make_from_backwards);
+        move_to_make_from_backwards = move_to_make_from_backwards + 1;
+        move_to_make.x = -move_to_make.x;
+        move_to_make.y = -move_to_make.y;
+        move_to_make.z = -move_to_make.z;
+    end
     % inference caly algorytm poruszania sie
     %musimy miec informacje gdzie jestesmy i dokad zmierzamy
     
@@ -83,7 +92,7 @@ while ( drone.energy > 0  && drone.if_return_to_start ~= true ) %dopóki energia 
         freezeTime = 3;
     else
         if (drone_state.radar_contact == true)
-            if (freezeTime == 0) %to znaczy ze w poprzedniej iteracji sie ruszylismy
+            if (freezeTime == 0) % to znaczy ze w poprzedniej iteracji sie ruszylismy
                 freezeTime = 3;
             end
             freezeTime = freezeTime - 1;
@@ -126,52 +135,6 @@ plot_simulation(environment, drone, moves_and_states); % prezentacja wyników po 
 
 [message] = analyze_results(drone, moves_and_states, simulation_time, message); %analiza rezultatów (dron w czasie)
 
-disp(message)
-
-%bazuje na tym ¿e mamy bazê wiedzy, mamy regu³y, po kolei przez te regu³y przeskakujemy i patrzymy co sie dzieje
-
-%najkrótsza œcie¿ka do pilota i sprawdza czy na tej najkrótszej œcie¿ce widzianej przez drona œcie¿ce
-%czy w tym horyzoncie na tej nakrótszej œcie¿ce coœ przeszkadza
-
-%mamy uaktualnian¹ bazê wiedzy (czy jesteœmy w polu ra¿enia, czy jesteœmy wykrywani przez radar)
-%na podstawie tego tworzymy sobie regu³y (regu³y s¹ odpowiednio sformu³owane)
-
-%jeœli jestesmy pod dzia³aniem radaru to porusz sie w prawo chyba ¿e jesteœ na granicy gun to nie poruszaj siê w prawo
-
-
-%speed to liczba komórek przez które mo¿na przejœæ.
-
-%Za³o¿enie pocz¹tkowe przód ty³ prawo lewo
-
-    
-   %% baza wiedzy
-   
-   %1.w bazie wiedzy musimy miec warunek ze jak znajdujemy sie nad pilotem
-   %jezeli jestem na pilotem i jestesm wyzej niz minimalna wysokosc to
-   %wtedy w dol
-   %2. Zrzucono ladunek -> powrot do miejsca startu
-   %3. Caly czas porusza sie przed siebie chyba ze jest jakies zagrozenie
-   %Jesli nie jestes pod dzialaniem zagrozenia i jestes nad dronem to wtedy
-   %ruch w przod
-   %4. Jezeli po zrobieniu kroku w przodu znalezlismy sie pod dzialaniem radaru to albo w lewo albo w prawo
-   %5. Jezeli po zrobieniu kroku w przodu znalezlismy sie pod dzialaniem
-   %wystrzalu to albo w lewo albo w prawo albo w gore
-   %5. Czy pole dzialania radaru nie konczy sie szybciej w prawo czy w lewo
-   %6. Probujemy zawsze isc w prawo a jak sie nie uda to w lewo
-   %7. Zestaw ograniczen na to ze dron jest w srodowisku
-   %(sytuacje w ktorych dojechal do konca osrodka)
-   %Czy jest na prawej, gornej granicy, dolnej, lewej (sprawdzanie
-   %wspolrzednych)
-   
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%System wnioskowania
-
-
-   
-  
-   
-   
-   
+disp(message)   
    
 
